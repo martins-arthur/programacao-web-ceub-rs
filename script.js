581b2445df738ea3348df3ac817804797e4808ea
martins-arthur <arthurmartins220903@gmail.com>
2024-03-21 09:12:54 -0300

    aula javascript

---
+++ script.js
@@ -0,0 +1,29 @@
+let var1 = "30";
+let var2 = 5;
+
+let resultado1 = var1 - var2;
+let resultado2 = var1 + var2; 
+
+console.log(resultado1);
+console.log(typeof(resultado1)); // 25
+
+console.log(resultado2);
+console.log(typeof(resultado2));
+
+
+
+
+
+
+
+
+/*
+let variavel1 = 0;
+variavel1 = "qualquercoisa" 
+
+const PI = 3.14;
+
+console.log(typeof(variavel1));
+
+PI = 3,15;
+*/
