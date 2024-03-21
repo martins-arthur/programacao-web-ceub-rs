d0893a26c930fa77a3a497639bbf174a6ea62889
martins-arthur <arthurmartins220903@gmail.com>
2024-03-21 10:26:17 -0300

    for loop javascript

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+let meuArray = ["cachorro", "gato", "coelho", "calopsita", "cobra", "dinossaouro", "vacalo"];
+
+for(let i = 0; i < 7; i++) {
+    console.log(meuArray[i]);
+}
+
+
+
+
+
+
+
+
+// let variavelArray = [5, 4, 8, 0, -7, "abc", 8.3];
+// console.log(variavelArray[5]);
+
+// let variavelObjeto = {
+//     nome: "Arthur",
+//     idade: "20",
+//     altura: "1.75",
+//     cpf: "0393278549",
+//     endereco: {
+//         rua: "abc",
+//         bairro: "xyz",
+//         numero: 1
+//     }
+// }
+
+// if(variavelObjeto.idade >= 18) {
+//     console.log("Pode entrar no meu site!");
+// } else {
+//     console.log("Não pode entrar no meu site!");
+// }
+
+//!= diferente
+//> maior
+//< menos
+//>= maior ou igual
+//<= menor ou igual
+
+//console.log(variavelObjeto.endereco);
+//console.log(variavelObjeto["nome"]);
+
+/*
 let var1 = "30";
 let var2 = 5;
 
@@ -9,13 +53,7 @@ console.log(typeof(resultado1)); // 25
 
 console.log(resultado2);
 console.log(typeof(resultado2));
-
-
-
-
-
-
-
+*/
 
 /*
 let variavel1 = 0;
