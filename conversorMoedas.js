81a3238726decd7d9fed31210044c14ee35017e8
martins-arthur <arthurmartins220903@gmail.com>
2024-04-04 10:26:26 -0300

    chicoMoedas

---
+++ conversorMoedas.js
@@ -0,0 +1,32 @@
+let valoresConversao = {
+    real: {
+        dolar: 0.27,
+        euro: 0.18
+    },
+    dolar: {
+        real: 5.03,
+        euro: 1.09
+    },
+    euro: {
+        real: 5.47,
+        dolar: 0.92
+    }
+}
+
+
+function converter() {
+    let valorUsuario = document.getElementById("valor-usuario").value;
+
+    let moedaOrigem = document.getElementById("moeda1").value;
+    let moedaDestino = document.getElementById("moeda2").value;
+
+    let conversao = valorUsuario * valoresConversao[moedaOrigem][moedaDestino];
+
+    let paragrafoResultado = document.getElementById("resultado");
+    paragrafoResultado.textContent = conversao;
+
+
+    //console.log(moedaOrigem);
+    //console.log(moedaDestino);
+    //console.log(valorUsuario);
+}
